d18616f83da61aaa0a7f372ba9bb4477efa3d911
Mateus Melo Oliveira <Mateus99Melo@gmail.com>
2022-08-29 11:59:28 -0300

    commit inicial

---
+++ Licao26-08IBMm/Licao26-08IBM/Calculo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Licao26_08IBM {
+
+    class Calculo {
+        public int Hora { get; }
+        public int Minuto { get;}
+        public int Resto { get; set; }
+
+        public Calculo(int hora, int minuto) {
+            Hora = hora;
+            Minuto = minuto;
+        }
+        public void TempoCalc() {
+
+            if (Hora < 8 && Minuto < 01 || Hora < 7) {
+                Console.WriteLine("Atraso maximo: " + 0);
+            }
+            else {
+                Resto = (Hora - 7) * 60 + Minuto;
+                Console.WriteLine("Atraso maximo: " + Resto);
+            }
+        }
+    }
+}
---
+++ Licao26-08IBMm/Licao26-08IBM/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Licao26_08IBM {
+    class Program {
+        static void Main(string[] args) {
+
+            Console.WriteLine("Digite o horário no formato 00:00");
+            string[] valores = Console.ReadLine().Split(':');
+            int hora = int.Parse(valores[0]);
+            int minuto = int.Parse(valores[1]);
+
+            Calculo calc = new Calculo(hora, minuto);
+
+            calc.TempoCalc();
+        }
+    }
+}
